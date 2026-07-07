b95038c21fa799a6e55681f0c8ccc1be7c56b49e
Fatoumata DRAME <dramefatoumata029@gmail.com>
2026-07-07 13:55:31 +0000

    Implementation des des fonctions move() et audit() en utilisant le decorateur @action

--- ecostock/serializers.py
+++ ecostock/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Warehouse, Product
+from django.db.models import Sum
 
 
 class WarehouseSerializer(serializers.ModelSerializer):
@@ -13,4 +14,18 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id','nom', 'quantite', 'warehouse', 'date_expiration', 'etat']
 
+     # Fonction pour Vérifier que l'entrepôt de destination n'a pas atteint sa capacité
+    def validate_warehouse(self, warehouse):
+        autres_produits = Product.objects.filter(warehouse=warehouse)
+        if self.instance:
+            autres_produits = autres_produits.exclude(pk=self.instance.pk)
+        
+        total_actuel = autres_produits.aggregate(Sum('quantite'))['quantite__sum'] or 0
+        quantite_a_ajouter = self.instance.quantite if self.instance else self.initial_data.get('quantite', 0)
 
+        if total_actuel + quantite_a_ajouter > warehouse.capacite:
+            raise serializers.ValidationError(
+                f"L'entrepôt '{warehouse.nom}' n'a pas assez de place (capacité : {warehouse.capacite} kg) alors que {self.instance.nom} a {self.instance.quantite} kg."
+            )
+
+        return warehouse
--- ecostock/views.py
+++ ecostock/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.response import  Response
 
 # Create your views here.
 from .models import Warehouse, Product
@@ -12,10 +13,44 @@ class WarehouseViewSet(viewsets.ModelViewSet):
     serializer_class = WarehouseSerializer
 
 
+    @action(detail = True, methods = ['get'])
+    def audit(self, request, pk=None):
+        warehouse = self.get_object()
+
+        produit= Product.objects.filter(warehouse=warehouse)
+        total_produit = produit.count()
+        return Response({
+            'entrepot': warehouse.nom,
+            'capacite': warehouse.capacite,
+            'total_produits': total_produit,
+        })
+
+
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-   
-        
+    @action(detail = True, methods =['post'])
+    def move(self, request, pk = None):
+        product = self.get_object()
+        # Vérifier que le produit n'est pas périmé
+        if product.etat == 'périmé':
+            return Response ({'error': "Produit périmé, transfert impossible."},
+                              status=400
+                            )
+        # Transfere d'un produit vers un autre entrepot, 
+        # ici l'instance du produit donne en premier parametre montre que ce n'est pas un crearion mais une modification, 
+        # data=request.data conteint les nouvelles donnees envoyer par le body et 
+        # partial=True montre c'est un PATCH pas un PUT 
+
+        # raise_exception=True nous permet d'eviter les if/else pour la gestion des erreurs
+
+        serializer = self.get_serializer(product, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+    
+
